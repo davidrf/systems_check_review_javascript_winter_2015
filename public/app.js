4ccf322b7ccba14858ed8203588f47a147b6a021
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2015-12-03 16:50:17 -0500

    finish clinic

--- public/app.js
+++ public/app.js
@@ -1,36 +1,23 @@
 $(document).ready(function() {
-  $('form').submit(function(event) {
-    // STEP 1: prevent the default action (form submission) from happening
-
-
-    // STEP 2: pull the question entered by the user in the form
-    // from the DOM
-
-
-    // STEP 3a: call the submitQuestionViaAjax function with the
-    // correct argument, if the form was filled out
-
-
-    // STEP 3b: show an alert message if the form is empty
-
-
+  $("form").on("submit", function(event){
+    event.preventDefault();
+    var title = $("#question_title").val();
+    if (title) {
+      submitQuestionViaAjax(title);
+    } else {
+      alert("stop that!");
+    }
   });
 });
 
 var submitQuestionViaAjax = function(title) {
-  // create ajax request
   var request = $.ajax({
-    method: "STEP-4-put-correct-http-verb-here",
-    url: "STEP-5-put-correct-path-here",
-    data: "STEP-6-put-data-here"
+    method: "POST",
+    url: "/questions",
+    data: { title: title }
   });
 
-  // if ajax request is successful, update the DOM
-  request.done(function() {
-    alert("Successful AJAX request! Now update the DOM");
-    // STEP 7a: comment out the line above
-    // STEP 7b: write code to update the DOM with the new question title
-
-
+  request.done(function(responseData) {
+    $("ul").append("<li>" + title + "</li>");
   });
 }
